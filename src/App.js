f27b00aa25f754f5427bbd46f3ba9c473cefa30f
Robert Cook <ender1117@yahoo.comgit>
2018-12-15 13:51:51 -0800

    added index.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,95 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./components/weather";
+import Form from "./components/form";
+import Titles from "./components/titles";
+//import Temp from "./components/temp";
+const Api_Key = "ab1c42024fc38bb21538762b2854b11a";
 
-class App extends Component {
-  render() {
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      city: '',
+      country:'',
+      units: 'metric',
+      temp: { current: '', max: '', min: '' },			
+      humidity: '',
+      description: '',
+      icon: ''
+
+    };
+
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleLocationChange = this.handleLocationChange.bind(this);
+    // this.handleToggle = this.handleToggle.bind(this);
+    // this.getWeather = this.getWeather.bind(this);
+  }    
+   
+      
+   //getWeather make the api call using -  async/await
+    getWeather = async (e) => {
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    /* const units = e.target.elements.units.value; */
+
+    e.preventDefault();
+    // async/await 
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}&units=metric`);
+    
+    /* convert to json */
+    const response = await api_call.json();
+    console.log(response);
+    
+    if (city && country) {
+      this.setState({
+        temperature: response.main.temp,
+        city: response.name,
+        country: response.sys.country,
+        humidity: response.main.humidity,
+        description: response.weather[0].description,
+        icon: response.weather[0].icon,
+        error: ""
+      })
+      } else {
+        this.setState({ error: "Please input search values..."})
+      }
+  }
+  
+   handleToggle(_event, isChecked) {
+      return isChecked ? this.setState({ units: 'metric' }) : this.setState({ units: 'imperial' });
+    }
+    
+    render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      <div>
+        <div className = "wrapper" >
+          <div className = "main" >
+            <div className = "container" >
+              <div className = "row" >
+               <div className = "col-lg-9 title-container" >
+                <Titles / >
+                 </div> 
+                    <div className = "col-lg-9 form-container" >  
+                      <Form loadWeather = {this.getWeather} /> 
+                            <Weather temperature = {this.state.temperature} 
+                                    city = {this.state.city}
+                                    country = {this.state.country} 
+                                    humidity = {this.state.humidity}
+                                    description = {this.state.description}
+                                    icon = {this.state.icon}
+                                    error = {this.state.error}  /> 
+                   </div> 
+               </div> 
+            </div> 
+          </div> 
+        </div> 
       </div>
-    );
+
+    )
   }
 }
-
 export default App;
